031ab4846a26208a11508300e44ec858695addbd
itachicross <edufmatos@gmail.com>
2017-12-08 20:24:53 -0200

    Update BuscaVetor.java

--- src/trabqualidadeteste/BuscaVetor.java
+++ src/trabqualidadeteste/BuscaVetor.java
@@ -3,7 +3,6 @@ package trabqualidadeteste;
 public class BuscaVetor {
 
     public static void busca(SearchType type, int[] itens, int size, int key, ResultType result, boolean ascending) {
-        int aux = 0;
         if (ascending) {
 
             for (int indice = 0; indice < size; indice++) {
@@ -13,18 +12,14 @@ public class BuscaVetor {
                     if (key == itens[indice]) {
                         result.ReturnType(ResultType.SearchResult.FoundExact, indice, itens[indice]);
                         break;
-                    } else {
-                        result.ReturnType(ResultType.SearchResult.NotFound, null, null);
                     }
+
                 }
                 if (type.equals(SearchType.GreaterThan)) {
 
                     if (itens[indice] > key) {
                         result.ReturnType(ResultType.SearchResult.FoundGreater, indice, itens[indice]);
                         break;
-                    } else {
-                        result.ReturnType(ResultType.SearchResult.NotFound, null, null);
-
                     }
 
                 }
@@ -36,21 +31,13 @@ public class BuscaVetor {
                     } else if (itens[indice] > key) {
                         result.ReturnType(ResultType.SearchResult.FoundGreater, indice, itens[indice]);
                         break;
-                    } else {
-                        result.ReturnType(ResultType.SearchResult.NotFound, null, null);
                     }
 
                 }
                 if (type.equals(SearchType.LessThan)) {
 
                     if (itens[indice] < key) {
-                        aux = itens[indice + 1];
-                        if (aux >= key) {
-                            result.ReturnType(ResultType.SearchResult.FoundLess, indice, itens[indice]);
-                            break;
-                        }
-                    } else {
-                        result.ReturnType(ResultType.SearchResult.NotFound, null, null);
+                        result.ReturnType(ResultType.SearchResult.FoundLess, indice, itens[indice]);
                     }
 
                 }
@@ -60,17 +47,10 @@ public class BuscaVetor {
                         result.ReturnType(ResultType.SearchResult.FoundExact, indice, itens[indice]);
                         break;
                     } else if (itens[indice] < key) {
-                        aux = itens[indice + 1];
-                        if (aux > key) {
-                            result.ReturnType(ResultType.SearchResult.FoundLess, indice, itens[indice]);
-                            break;
-                        }
-                    } else {
-                        result.ReturnType(ResultType.SearchResult.NotFound, null, null);
+                        result.ReturnType(ResultType.SearchResult.FoundLess, indice, itens[indice]);
                     }
 
                 }
-
             }
 
         } else {
@@ -78,24 +58,17 @@ public class BuscaVetor {
             for (int indice = size - 1; indice >= 0; indice--) {
 
                 if (type.equals(SearchType.Equals)) {
+
                     if (key == itens[indice]) {
                         result.ReturnType(ResultType.SearchResult.FoundExact, indice, itens[indice]);
                         break;
-                    } else {
-                        result.ReturnType(ResultType.SearchResult.NotFound, null, null);
                     }
+
                 }
                 if (type.equals(SearchType.GreaterThan)) {
 
                     if (itens[indice] > key) {
-                        aux = itens[indice - 1];
-                        if (aux <= key) {
-                            result.ReturnType(ResultType.SearchResult.FoundGreater, indice, itens[indice]);
-                            break;
-                        }
-                    } else {
-                        result.ReturnType(ResultType.SearchResult.NotFound, null, null);
-
+                        result.ReturnType(ResultType.SearchResult.FoundGreater, indice, itens[indice]);
                     }
 
                 }
@@ -105,13 +78,7 @@ public class BuscaVetor {
                         result.ReturnType(ResultType.SearchResult.FoundExact, indice, itens[indice]);
                         break;
                     } else if (itens[indice] > key) {
-                        aux = itens[indice - 1];
-                        if (aux < key) {
-                            result.ReturnType(ResultType.SearchResult.FoundGreater, indice, itens[indice]);
-                            break;
-                        }
-                    } else {
-                        result.ReturnType(ResultType.SearchResult.NotFound, null, null);
+                        result.ReturnType(ResultType.SearchResult.FoundGreater, indice, itens[indice]);
                     }
 
                 }
@@ -120,8 +87,6 @@ public class BuscaVetor {
                     if (itens[indice] < key) {
                         result.ReturnType(ResultType.SearchResult.FoundLess, indice, itens[indice]);
                         break;
-                    } else {
-                        result.ReturnType(ResultType.SearchResult.NotFound, null, null);
                     }
 
                 }
@@ -133,15 +98,10 @@ public class BuscaVetor {
                     } else if (itens[indice] < key) {
                         result.ReturnType(ResultType.SearchResult.FoundLess, indice, itens[indice]);
                         break;
-                    } else {
-                        result.ReturnType(ResultType.SearchResult.NotFound, null, null);
                     }
 
                 }
-
             }
-
         }
-
     }
 }
